ed4367d9f72e330c8237ab8e5364b107258a701c
Charles Larzillière <charles@MBP-de-Charles.localdomain>
2023-11-23 16:43:19 +0100

    feat: 2.2 + 2.3 - front - ajout de filtre et persistance en QueryParams

--- technical-test-renderer/src/main/java/technical/test/renderer/controllers/TechnicalController.java
+++ technical-test-renderer/src/main/java/technical/test/renderer/controllers/TechnicalController.java
@@ -7,8 +7,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import reactor.core.publisher.Mono;
 import technical.test.renderer.facades.FlightFacade;
+
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Controller
 @RequestMapping
@@ -22,6 +26,21 @@ public class TechnicalController {
     @GetMapping
     public Mono<String> getMarketPlaceReturnCouponPage(final Model model) {
         model.addAttribute("flights", this.flightFacade.getFlights());
+        return Mono.just("pages/index");
+    }
+
+    @GetMapping("/filtrage")
+    public Mono<String> getMarketPlaceReturnCouponPageWithFilter(final Model model,
+                                                                 @RequestParam Optional<Integer> minPrice,
+                                                                 @RequestParam Optional<Integer> maxPrice,
+                                                                 @RequestParam Optional<String> originIata,
+                                                                 @RequestParam Optional<LocalDateTime> earliestDepartureDate) {
+        model.addAttribute("flights", this.flightFacade.getFlights()
+                .filter((ff) -> minPrice.isEmpty() || ff.getPrice() >= minPrice.get())
+                .filter((ff) -> maxPrice.isEmpty() || ff.getPrice() <= maxPrice.get())
+                .filter((ff) -> (originIata.isEmpty() || originIata.get().isEmpty())  ? true : originIata.get().equals(ff.getOrigin().getIata()))
+                .filter((ff) -> earliestDepartureDate.isEmpty() || ff.getDeparture().isAfter(earliestDepartureDate.get()))
+        );
         return Mono.just("pages/index");
     }
 }
